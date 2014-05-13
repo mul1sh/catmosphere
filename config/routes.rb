dc21d826a3ebf16127ab0551a9cd75cb0498d429
Constantin Hofstetter <Consti@Consti.de>
2014-05-13 15:53:10 +0200

    set root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome/root'
+  root 'welcome#root'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
