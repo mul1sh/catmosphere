7e2049cf2d7bb7b81440a1cfe5a4af796e359997
Constantin Hofstetter <Consti@Consti.de>
2014-05-27 19:51:26 +0200

    shorter team texts #66

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,9 +9,7 @@ module ApplicationHelper
             email: "david@chinkoproject.com",
             image: "david-simpson.jpg",
             cv: <<-HTML
-David has worked 5 years in business management in the UK before joining Central African Wildlife Adventures in 2010. He was its General Manager and pilot, working in what is today the Chinko Project Area.
-
-David has worked with the Friedkin Conservation Fund (2013) in different wildlife protected areas throughout Tanzania, and co-founded the Chinko Project, which he is directing today
+David worked in business management in the UK before joining the management of Central African Wildlife Adventures. He is co-founder and director of the Chinko Project.
 HTML
           },
           {
@@ -20,11 +18,7 @@ HTML
             email: "raffael@chinkoproject.com",
             image: "raffael-hickisch.jpg",
             cv: <<-HTML
-Raffael studied Social Ecology and Computer Science. He did research (mainly sustainability science) in Senegal and Kenya where he received a research fellowship.
-
-He was awarded the Panthera Kaplan grant for the field research in the Chinko/Mbari drainage basin, that he and Thierry Aebischer conducted since 2012. Even though he runs <a href='forscherei.at'>forscherei.at</a>, an institution supporting young researchers, most of his scientific activity concerns the Chinko Area.
-
-At Chinko Project he is today responsible for fundraising, and project development, and supports operations as vice president.
+Raffael came to the Chinko/Mbari drainage basin for research in 2012. He is responsible for fundraising, and project development, and supports operations as vice president.
 HTML
           },
           {
@@ -33,9 +27,7 @@ HTML
             email: nil,
             image: "federico-lambert.jpg",
             cv: <<-HTML
-Born in La Pampa in Argentina, Federico has always had a passion for the wild. Graduated with a bachelors in Economics and accounting, Federico was very fast at establishing several companies. Before the age of 30, Federico had already faced more management and business related questions then most people do.
-
-In 2012 he got involved in the Chinko Project, where he is today managing and overseeing the local administration and logistics inside the Chinko Project Area.
+Federico has always had a passion for the wild; and economy: He is overseeing the local administration and logistics in Chinko.
 HTML
           },
           {
@@ -44,9 +36,7 @@ HTML
             email: nil,
             image: "eric-ngamana.jpg",
             cv: <<-HTML
-Having grown up in both France and CAR, Eric Ngamana completed his studies in Paris before moving to CAR. Working many years as a full time accountant and logistical manager for a number of large safari operators, he previously managed one of the most renowned revisory firms in Bangui and has also been active in the large transport business.
-
-He is supervising bureaucratic processes in the Bangui Headquarters.
+Eric has been working many years as a reviser, accountant and logistical manager in Bangui. At Chinko Project he is supervising bureaucratic processes in Bangui.
 HTML
           },
           {
@@ -55,9 +45,7 @@ HTML
             email: "tobias@chinkoproject.com",
             image: "tobias-ruckstuhl.jpg",
             cv: <<-HTML
-Having spent several years of his childhood in CAR, Tobias Ruckstuhl studied International Affairs and Governance, and Business Economics. He holds the International Management Accountant (IMA CMA) certification. Growing up in the USA and Africa, he is fluent in English, French and Sango as well as his native language, German.
-
-2011 he started as the economic manager of Central African Wildlife Adventures where, besides building up a professional accounting system, he was the responsible for all finances, logistics and personnel. Since 2013 he is responsible for the budget, audit and control of finances of the Chinko Project.
+Tobias spent his childhood in CAR, and the US. He holds the International Management Accountant (IMA CMA), and since 2013 he is responsible for the budget, audit and control of finances of the Chinko Project.
 HTML
           },
           {
@@ -66,9 +54,7 @@ HTML
             email: "emelie@chinkoproject.com",
             image: "emelie-mararv.jpg",
             cv: <<-HTML
-Spending half of her life and childhood in CAR Emelie collected invaluable knowledge and experience of Central African culture. Excellent skills in Sango language, together with a genuine concern for the country based in a sense of belonging, enable Emelie to work in Community Development projects that foster long lasting and sustainable development.
-
-She has ever since been involved into helping grassroots development initiatives, and provides support in management and fundraising. Since 2012 Emelie follows the Peace and Development program at the University of Uppsala.
+Emelie spent half her life and childhood in CAR. She is highly knowledgeable of Central African culture and has ever since been involved into helping grassroots development initiatives.
 HTML
           }
         ],
@@ -88,9 +74,7 @@ HTML
             email: nil,
             image: "loredana-hartmann.jpg",
             cv: <<-HTML
-Loredana Hartmann works on her degree in law, and regularly participates in 'Model United Nations' conferences, to gather experiences with diplomacy. Since 2005 she supports the 'Papiliorama' - a zoological garden in Switzerland with nature conservation projects in Belize, Central America, and the intent to sensitize Swiss and Europeans for the value of nature and the necessity to protect it, as well as to raise funds for the conservation areas.
-
-Since 2013 she advises Chinko Project in legal issues whenever needed.
+Loredana works on her degree in law, and regularly participates in 'Model United Nations' conferences, to gather experiences with diplomacy. Since 2013 she advises Chinko Project in legal issues whenever needed
 HTML
 
           },
@@ -100,9 +84,7 @@ HTML
             email: nil,
             image: "milena-klimek.jpg",
             cv: <<-HTML
-Milena Klimek does her PhD on Organic Farming, and Transdisciplinary Systems Research. Her undergraduate work was completed in the  Environmental Studies, Sustainable Development and Alternative Agriculture disciplines. She has had various positions in conservation work, predominantly in North America, focusing on both terrestrial and aquatic restoration and also for The Nature Conservancy MN.
-
-She currently supports many parties with language review and proposal writing and particularly enjoys assisting The Chinko Project in ventures such as research papers and grant proposals.
+Milena does her PhD on Organic Farming, and Transdisciplinary Systems Research. She currently supports many parties with language review and proposal writing and particularly enjoys assisting the Chinko Project.
 HTML
 
           },
@@ -112,9 +94,7 @@ HTML
             email: nil,
             image: "ryan-youngblood.jpg",
             cv: <<-HTML
-Ryan Youngblood was born and raised in Texas spending most of his time hunting, fishing, and exploring. While studying at university he took a trip to East Africa that unearthed his love for travel and served as a catalyst for his filmmaking career. As soon as Ryan graduated, he moved to Rwanda and became a freelance videographer. Since freelancing, he has traveled to over 50 countries filming for NGO's and news corporations.
-
-He's worked on award-winning documentaries and has produced media packages for the BBC, the Associated Press, and CNN. His first trip to CAR was with the Chinko Project in March of 2014, and since then supports the project in film and photography.
+Ryan is freelance videographer, and has worked on award-winning documentaries and has produced media packages for the BBC, the Associated Press, and CNN. Since 2014, he supports the project in film and photography.
 HTML
 
           }
@@ -126,9 +106,7 @@ HTML
             email: nil,
             image: "jean-marc-froment.jpg",
             cv: <<-HTML
-Jean Marc graduated at the University of Liege (Belgium) where he got his MSc in biology and specialization in ecology and wildlife management. He has since devoted his life to wildlife conservation in Africa with a special interest in big, remote and sparsely populated ecosystems. He began his career in 1981 working for FAO and then for the European Commission on the management of the protected areas of Northern CAR.
-
-Jean Marc joined African Parks in 2005, where he today serves as Conservation Director. He learned about the Chinko Project in 2012, and since then supports the board in taking the necessary steps for long term conservation.
+Jean Marc has devoted his life to wildlife conservation in Africa with a special interest in big, remote and sparsely populated ecosystems. Jean Marc joined African Parks in 2005, where he today serves as Conservation Director.
 HTML
           },
           {
@@ -137,9 +115,7 @@ HTML
             email: nil,
             image: "thierry-aebischer.jpg",
             cv: <<-HTML
-Thierry completed the Master of Science in Ecology and Evolution at the University of Bern and EAWAG Lucerne. He worked in Nki Boumba Bek National Park (Cameroon), and since 2011 officially advices on the bird wildlife in the Regional Nature Park Gantrisch (Switzerland).
-
-He is jury member of the International Biology Olympiad IBO, co-organizer of the Swiss Biology Olympiad, and has since 2012 coordinated several expeditions to the Chinko/Mbari drainage basin in CAR. This generated data that is today used as baseline for his PhD and the conservation work at Chinko Project, which he co-founded.
+Thierry completed the Master of Science in Ecology and Evolution, and is currently working on his PhD at the University of Fribourg. Since 2012 he coordinated several expeditions to the Chinko, collecting data which is today the baseline for his PhD and the conservation work at the Chinko Project.
 HTML
           },
           {
@@ -148,9 +124,7 @@ HTML
             email: nil,
             image: "david-zokoe.jpg",
             cv: <<-HTML
-Retired from the Central African government where he rendered service in a number of minister and director posts, David Zokoe has always been implicated in charity and is burning to bring change to the situation for the people of his very poor country. David presides international NGO’s based in CAR, that are currently working on agricultural education as well as medical help for remote villages in the country. David is also helping to manage a foster home for displaced children and providing them with educational services in the capital.
-
-Since 2013 he dedicates his invaluable experience and knowledge of Central African bureaucracy to carry through the set up Chinko Project he co-founded.
+David retired from the Central African government, where he rendered service in a number of minister and director posts. He is burning to bring change to the situation for the people of his very poor country, and since 2013 he dedicates his invaluable experience to the Chinko Project.
 HTML
           },
           {
@@ -159,9 +133,7 @@ HTML
             email: nil,
             image: "erik-mararv.jpg",
             cv: <<-HTML
-Erik owns the safari company Central African Wildlife Adventures, together with his wife Emelie. Born and raised in the CAR, he has decade long experience in guiding safaris in CAR.
-
-Since then, Erik has an extensive network in the country, and profound knowledge of administrative processes in Central Africa - without which the Chinko Project could not have been founded.
+Erik owns the safari company Central African Wildlife Adventures. Erik has an extensive knowledge of the region, and a profound network in Central Africa - without which the Chinko Project could not have been founded.
 HTML
           }
         ],
@@ -173,9 +145,7 @@ HTML
             email: nil,
             image: "jean-baptiste-mamang.jpg",
             cv: <<-HTML
-Jean Baptiste has been involved in the conservation of the Central African Wildlife for more than two decades. After finishing his studies in West Africa he started working by the early 90’s for the EU project ‘Developpement de la Region du Nord de la Centrafrique’. He held a number of positions in this domain, including base manager, anti poaching coordinator and logistical manager.
-
-He was Director of Wildlife for the Central African Government from 2007 until 2014, and ever since makes a fierce effort to defend and promote a vanishing gem in the heart of Africa. He has advised the Chinko Project, where he has been involved from the start as co-founder.
+Jean Baptiste has been involved in the conservation of the Central African Wildlife for more than two decades. He was Director of Wildlife for the Central African Government from 2007 until 2014, and has advised the Chinko Project from the start.
 HTML
 
           },
@@ -185,9 +155,7 @@ HTML
             email: nil,
             image: "philipp-henschel.jpg",
             cv: <<-HTML
-Starting in 2009, Philipp Henschel joined Panthera full-time as the Lion Program Regional Coordinator within West and Central Africa. Originally from Germany, Philipp is now based in Gabon, where he has lived for more than ten years, and from where he currently fulfills his position as Panthera’s Lion Survey Coordinator.
-
-Most recently, Philipp conducted lion field surveys in the Sahelian savannahs of Nigeria and Côte d’Ivoire, and further surveys are planned in Senegal, the Democratic Republic of Congo and Tanzania.
+Philipp is Lion Program Regional Coordinator at Panthera. He is now based in Gabon, where he has lived for more than ten years, and most recently conducted lion field surveys in the Sahelian savannahs of Nigeria and Côte d’Ivoire.
 HTML
           },
           {
@@ -196,9 +164,7 @@ HTML
             email: nil,
             image: "louisa-lombard.jpg",
             cv: <<-HTML
-Louisa is a Ciriacy-Wantrup postdoctoral fellow in the department of geography at the University of California at Berkeley. In July 2014 she will begin a position as assistant professor of sociocultural anthropology at Yale University. She earned her PhD in cultural anthropology from Duke University. Her dissertation is based on extensive field research in CAR and examines how actors in eastern CAR negotiate authority over resources and people.
-
-She first conducted research in CAR in 2003, and has since then worked in CAR as a field consultant to several international organizations, including Human Rights Watch, Small Arms Survey, Refugees International, and the World Bank, as well as an independent researcher.
+Louisa is a Ciriacy-Wantrup postdoctoral fellow at the University of California. In July 2014 she will begin a position as assistant professor of sociocultural anthropology at Yale University. She first conducted research in CAR in 2003, and has since then worked in CAR as a field consultant, as well as an independent researcher.
 HTML
           },
           {
@@ -207,9 +173,7 @@ HTML
             email: nil,
             image: "charlotte-mararv.jpg",
             cv: <<-HTML
-Charlotte has spent most her life in Central African Republic, working over seven years with tourism in the country often in close relation to the Central African government. Today she is involved and implicated in a number of different private companies and international NGO’s in the country, and also is the Swedish Consul to the Central African Republic.
-
-She has travelled in most of the country, and is particularly attached to the Eastern parts of CAR.
+Charlotte has spent most her life in Central African Republic and travelled most of the country. She works in the tourism sector, and is today implicated in a number of different private companies and international NGO’s. She also is the Swedish Consul to the Central African Republic.
 HTML
           },
           {
@@ -218,9 +182,7 @@ HTML
             email: nil,
             image: "racey-bingham.jpg",
             cv: <<-HTML
-Racey is a farmer and development consultant who splits her time between her farm project <a href='http://reberrockfarm.com' target='_blank'>reberrockfarm.com</a> and consultancy in Africa. She owns and operates Reber Rock Farm in the Adirondack Mountains of New York state. Her love of farming grew out of 10 years spent working in agricultural and rural development in West and Central Africa.
-
-In her consulting work (mainly UN; World Bank) she specializes in food security; draft power; diversified agricultural production; pastoral livelihoods; project design, implementation and monitoring; donor/client relation; and economic recovery. Her years long field experience provides her with great insight and awareness of the cultural and social obstacles to rural development in Central Africa.
+Racey is a farmer and development consultant who splits her time between her farm (reberrockfarm.com) and consultancy in Africa. In her consulting work (mainly UN; World Bank) food security and pastoral livelihoods are among her specialisations.
 HTML
           },
           {
@@ -229,9 +191,7 @@ HTML
             email: nil,
             image: "john-michael-fay.jpg",
             cv: <<-HTML
-Michael Fay graduated in 1978 from the University of Arizona, and then joined the Peace Corps working in Tunisia and the Central African Republic. He completed his doctorate on the western lowland gorilla in 1997, while also surveying large forest blocks by aeroplane and working to create and manage the Dzanga-Sangha park and the Nouabalé-Ndoki National Park in the Central African Republic and Republic of the Congo.
-
-Michael has worked for the Wildlife Conservation Society since 1990, and was an Explorer in Residence at the National Geographic Society. Since 2006, Michael has been very actively supporting conservation work in Central Africa, particularly Chad and Gabon. He is asides many other things, currently working to establishing an ‘Agence Nationale pour les Parcs Nationaux’ (ANPN) in CAR.
+Michael works for the Wildlife Conservation Society since 1990, and was an Explorer in Residence at the National Geographic Society. He has been very actively supporting conservation work in Central Africa, and is asides many other things, currently working to establish an ‘Agence Nationale pour les Parcs Nationaux’ in CAR.
 HTML
           },
           {
@@ -240,9 +200,7 @@ HTML
             email: nil,
             image: "anton-lundkvist.jpg",
             cv: <<-HTML
-Having lived and worked more than 15 years in Central African Republic Anton is currently completing his MSc in GIS/Geomatics at Lunds University in Sweden. Asides being on the Chinko Project board. he is working in a selection of land use management projects in Kenya, Tanzania and Uganda.
-
-As of his decades long experience in Africa, Anton is proficient in Swedish, English, French and Sango and speaks Swahili as well as Spanish.
+Anton lived and worked more than 15 years in Central African Republic. He studies GIS/Geomatics, and asides being on the Chinko Project board. he is working in a selection of land use management projects in Kenya, Tanzania and Uganda.
 HTML
           }
 
