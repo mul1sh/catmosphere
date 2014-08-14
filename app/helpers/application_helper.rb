acb9b1cd120fef391cd3b041836fe46a9427fb78
Bernhard Mueller <bernhard.mueller11@gmail.com>
2014-08-14 22:25:08 +0700

    add google map iframe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,17 +2,59 @@ module ApplicationHelper
 
     def team
       {
-        management: [
+        row1: [
           {
             name: "Apollo",
-            title: "CEO, Founder",
-            email: "david@chinkoproject.com",
+            title: "Admiral",
             image: "01-apollo.png",
             cv: <<-HTML
-David worked in business management in the UK before joining the management of Central African Wildlife Adventures. He is co-founder and director of the Chinko Project.
-HTML
+              Apollo is the old guy in the team.
+              HTML
           },
-
+          {
+            name: "Obi Wan",
+            title: "Admiral",
+            image: "03-obiwan.png",
+            cv: <<-HTML
+              Apollo is the old guy in the team.
+              HTML
+          },
+        ], 
+        row2: [
+          {
+            name: "Luke",
+            title: "Jedi Knight",
+            image: "02-luke.png",
+            cv: <<-HTML
+              Apollo is the old guy in the team.
+              HTML
+          },
+          {
+            name: "Valentina",
+            title: "Jedi Knight",
+            image: "04-wawa.png",
+            cv: <<-HTML
+              Apollo is the old guy in the team.
+              HTML
+          },
+        ],
+        row3: [
+          {
+            name: "Ace",
+            title: "Space Pilot",
+            image: "05-ace.png",
+            cv: <<-HTML
+              Apollo is the old guy in the team.
+              HTML
+          },
+          {
+            name: "Ming",
+            title: "Princess",
+            image: "06-ming.png",
+            cv: <<-HTML
+              Apollo is the old guy in the team.
+              HTML
+          },
         ],
       }
   end
