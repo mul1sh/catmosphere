3977321c3e8335c33cf28635c7d88f9fb7565334
Bernhard Mueller <bernhard.mueller11@gmail.com>
2014-08-16 12:36:56 +0700

    finish up cat descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
             title: "Cuddle Princess | American Shorthair",
             image: "06-ming.png",
             cv: <<-HTML
-              A princess from planet Aldeeran, Ming found a new home at Catmosphere Café when her planet was destroyed. Her beauty is unmatched in the Galaxy.
+              A princess from a planet far away, Ming found a new home at Catmosphere after her home planet was destroyed. Her beauty is unmatched in the Galaxy - a fact that she uses to her advantage.
               <small>
               Cuddly: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
               Playful: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
@@ -22,7 +22,7 @@ module ApplicationHelper
             title: "Space Pilot | Bengal Cat",
             image: "05-ace.png",
             cv: <<-HTML
-              Due to his fearless and sometimes rude behaviour this top pilot may seem inconsiderate to some. But a deep look into his eyes reveals a smart and caring mind.
+              Due to his fearless and sometimes rude behaviour this top pilot may seem inconsiderate to some. But a look into his eyes reveals a smart and caring mind.
               <small>
               Cuddly: <i class="fa fa-star"></i>
               Playful: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
@@ -53,74 +53,116 @@ module ApplicationHelper
               As the most experienced combatant Apollo is highly respected by the team. He is a fair leader, but will crack down swiftly on anyone who dares to challenge his power.
               <small>
               Cuddly: <i class="fa fa-star"></i>
-              Playful: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
-              Wild: <i class="fa fa-star"></i> <i class="fa fa-star"></i><i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
-              Sleepy: <i class="fa fa-star"></i>
+              Playful: <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+              Wild: <i class="fa fa-star"></i> 
+              Sleepy: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
               </small>
               HTML
           },
           {
             name: "Buzz",
-            title: "Admiral",
+            title: "Smuggler | British Shorthair",
             image: "10-buzz.png",
             cv: <<-HTML
-              Apollo is the old guy in the team.
+              A talended pilot, Buzz can keep his calm in even the most daunting situations. His cool demeanor gives him an advantage in space combat but also makes him seem "cold" in relationships.
+              <small>
+              Cuddly: <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+              Playful: <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+              Wild: <i class="fa fa-star"></i> 
+              Sleepy: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+              </small>              
               HTML
           },
           {
             name: "Obi Wan",
-            title: "Admiral",
+            title: "Jedi Master | Maine Coon + Chinchilla Cat",
             image: "03-obiwan.png",
             cv: <<-HTML
-              Apollo is the old guy in the team.
+              Obi Wan mastered the art of the Jedi at a young age. A powerful Jedi, he finds himself in a constant struggle against the dark side of the force.
+              <small>
+              Cuddly: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+              Playful: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+              Wild: <i class="fa fa-star"></i> <i class="fa fa-star"></i><i class="fa fa-star"></i> 
+              Sleepy: <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+              </small>
               HTML
           },
           {
             name: "Luke",
-            title: "Jedi Knight",
+            title: "Padawan | Scottish Fold",
             image: "02-luke.png",
             cv: <<-HTML
-              Apollo is the old guy in the team.
+              A beautiful young man from a desert planet. To deceive his enemies, he hides his incredible Jedi powers behind slow and awkward movements.
+              <small>
+              Cuddly: <i class="fa fa-star"></i> 
+              Playful: <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+              Wild: <i class="fa fa-star"></i> 
+              Sleepy: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+              </small>
               HTML
           },
           {
             name: "Leia",
-            title: "Jedi Knight",
+            title: "Padawan | Scottish Fold",
             image: "12-leia.png",
             cv: <<-HTML
-              Apollo is the old guy in the team.
+              Luke's sister has long fallen to the dark side of the force. But deep inside (and unknown to herself) the spark of love for her brother is still glowing.
+              <small>
+              Cuddly: <i class="fa fa-star"></i> 
+              Playful: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+              Wild: <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+              Sleepy: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+              </small>
               HTML
           },
           {
             name: "Valentina",
-            title: "Jedi Knight",
+            title: "Little Princess | Persian",
             image: "04-wawa.png",
             cv: <<-HTML
-              Apollo is the old guy in the team.
+              Valentina's primary weapon is her cuteness and she uses it to full effect. She likes dancing, prodding people with her paws, and filling the Galaxy with joy.
+              <small>
+              Cuddly: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+              Playful: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+              Wild: <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+              Sleepy: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+              </small>
               HTML
           },
           {
             name: "Boba",
-            title: "Princess",
+            title: "Headhunter | Persian",
             image: "08-boba.png",
             cv: <<-HTML
-              Apollo is the old guy in the team.
+              A merciless headhunter from a galaxy far, far away. Many have been fooled by harmless appearance - a bitter mistake that yields harsh consequences.
+              Cuddly: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+              Playful: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+              Wild: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+              Sleepy: <i class="fa fa-star"></i> <i class="fa fa-star"></i>
               HTML
           },
           {
             name: "Luna",
-            title: "Princess",
+            title: "Magician | Thai Cat",
             image: "11-luna.png",
             cv: <<-HTML
-              Apollo is the old guy in the team.
+              Luna is the lone survivor of an ancient alien race. She has the skill of enchanting the hearts of fellow cats using her magical stare and is usually found cuddled up with a cat friend. 
+              Cuddly: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+              Playful: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+              Wild: <i class="fa fa-star"></i> 
+              Sleepy: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
               HTML
           },
           {
             name: "Buttercup",
-            title: "Princess",
+            title: "Spy | Persian",
             image: "09-buttercup.png",
             cv: <<-HTML
-              Apollo is the old guy in the team.
+              A masterful spy, Buttercup has deserted to Catmosphere from an enemy space ship. He is in a permanent undercover mission but we cannot go into details on our website.
+              Cuddly: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+              Playful: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+              Wild: <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+              Sleepy: <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
               HTML
           },
         ],
